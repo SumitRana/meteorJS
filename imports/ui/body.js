da132c2ce2b35cd1fae3dba55c65f2263b21d032
SumitRana <rana.sumit93@gmail.com>
2016-04-24 17:22:30 +0530

    Assignment Commit

---
+++ imports/ui/body.js
@@ -0,0 +1,28 @@
+import { Template } from 'meteor/templating';
+import './body.html';
+import { Tasks } from '../api/tasks.js';
+import { ReactiveDict } from 'meteor/reactive-dict';
+
+Template.body.helpers({
+	tasks(){
+		return Tasks.find({});
+	},
+});
+
+Template.body.events({
+	'submit .new-task'(event){
+		event.preventDefault();
+
+		const target = event.target;
+		const comment = target.text.value;
+		console.dir(Meteor.user());
+		Tasks.insert({
+			comment,
+			createdate: new Date(),
+			owner: Meteor.userId(),
+			email: Meteor.user().emails[0].address,
+		});
+
+		target.text.value = '';
+	},
+});
